091f9f4668badca65128078786bf7c47317c97e0
ujwal-kumar <ujwal.prodevans@gmail.com>
2017-10-18 12:20:22 +0530

    Update TestRun.java

--- src/test/java/com/pack/home/TestExecution/TestRun.java
+++ src/test/java/com/pack/home/TestExecution/TestRun.java
@@ -16,7 +16,8 @@ public class TestRun {
 
 	@BeforeTest
 	public void executing() throws InterruptedException, IOException {
-		driver = Dlib.openBrowser("chrome");
+//		driver = Dlib.openBrowser("chrome");
+		driver = Dlib.openBrowser("FireFox");
 		lib = new LibraryForGenericFunction(driver);
 		// Application link will be opened
 		driver.get("http://13.92.196.16/YouthInc");
