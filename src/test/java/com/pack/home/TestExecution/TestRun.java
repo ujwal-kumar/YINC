c68a582693cd839bba51bc7b8cebac4892b958a1
ujwal-kumar <ujwal.prodevans@gmail.com>
2017-10-26 15:04:44 +0530

    Update TestRun.java

--- src/test/java/com/pack/home/TestExecution/TestRun.java
+++ src/test/java/com/pack/home/TestExecution/TestRun.java
@@ -19,43 +19,33 @@ public class TestRun {
 		driver = Dlib.openBrowser("chrome");
 		lib = new LibraryForGenericFunction(driver);
 		// Application link will be opened
-		driver.get("http://192.168.1.39/YouthInc");
+		driver.get("http://69.176.98.99:9356/admin/");
 	}
 	
-	
+	@Test(priority = 1, description="Performs an successful login and checks the dashboard url (passes)")
+    public void login() throws InterruptedException, IOException {
+        lib.jsSendKeysForID("loginform-email", "loginform-email", "login", "ID");
+        lib.jsSendKeysForID("loginform-password", "loginform-password", "login", "ID");
+        Assert.assertTrue(lib.waitAndClickForID("login", "login", "ID"));
+        Thread.sleep(2000);
+        //Assert.assertEquals(lib.getCurrentUrl(), "");                    
+    }
 
-	@Test(priority = 2, description="Navigate to forgot Password page")
+	/*@Test(priority = 2, description="Navigate to forgot Password page")
 	public void passwdlink() throws InterruptedException, IOException {
 		lib.waitAndClickForID("passwdlink", "login", "class");
 	}
 	
-	@Test(priority = 6, description="Enter mail address page ")
+	@Test(priority = 3, description="Enter mail address page ")
 	public void emaillink() throws InterruptedException, IOException {
 		lib.jsSendKeysForID("emaillink", "abcd123@gmail.com", "login", "ID");
 		
 		Assert.assertTrue(lib.waitAndClickForID("emaillink2", "login", "name"));
 		Thread.sleep(2000);
 		//Assert.assertEquals(lib.getCurrentUrl(), "http://marcus.oneeight.co.in/zeno/dashboard");					
-	}
+	}*/
 	
-	/*
-	 * 
-	
-	
-	
-	@Test(priority = 6, description="Perform Url Blocking in custom  (passes)")
-	public void customlink2() throws InterruptedException, IOException {
-		lib.jsSendKeysForID("url", "www.facebook.com", "dashboard", "ID");
 		
-		Assert.assertTrue(lib.waitAndClickForID("customlink2", "dashboard", "ID"));
-		Thread.sleep(2000);
-		//Assert.assertEquals(lib.getCurrentUrl(), "http://marcus.oneeight.co.in/zeno/dashboard");					
-	}
-	*/
-	
-	
-	
-	
 	@AfterTest()
 	public void afterMethod() throws IOException {
 		driver.quit();		
